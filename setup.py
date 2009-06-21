12fb3800bc7ae98f875c4bd42ac57fcd4a2b3ae2
Zachary Voase <zacharyvoase@me.com>
2009-06-22 01:55:01 +0200

    Fixed setup for new URL; bumped to v0.2
    committer: Zachary Voase <zacharyvoase@me.com>
    
    --HG--
    extra : convert_revision : 42feb2c8373d9ff8a6a8d1ee58f1571df99425d9

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from distutils.core import setup
 
 setup(
     name='URLObject',
-    version='0.1.1',
+    version='0.2',
     description='A utility class for manipulating URLs.',
     author='Zachary Voase',
-    author_email='disturbyte@gmail.com',
-    url='http://github.com/disturbyte/urlobject',
+    author_email='zacharyvoase@me.com',
+    url='http://github.com/zacharyvoase/urlobject',
     py_modules=['urlobject'],
 )
