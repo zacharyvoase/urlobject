1aecdda7b975e8edefc201a2e9ffdca477cce69a
Zachary Voase <z@zacharyvoase.com>
2012-02-14 22:02:40 +0000

    Reimplemented parse_qsl for QueryString.list to handle empty parameters.

--- lib/urlobject/query_string.py
+++ lib/urlobject/query_string.py
@@ -1,4 +1,5 @@
 import collections
+import re
 import urlparse
 
 
@@ -6,7 +7,31 @@ class QueryString(unicode):
 
     @property
     def list(self):
-        return urlparse.parse_qsl(self.encode('utf-8'))
+        result = []
+        if not self:
+            # Empty string => empty list.
+            return result
+
+        name_value_pairs = re.split(r'[\&\;]', self)
+        for name_value_pair in name_value_pairs:
+            # Split the pair string into a naive, encoded (name, value) pair.
+            name_value = name_value_pair.split('=', 1)
+            # 'param=' => ('param', None)
+            if len(name_value) == 1:
+                name, value = name_value + [None]
+            # 'param=value' => ('param', 'value')
+            # 'param=' => ('param', '')
+            else:
+                name, value = name_value
+
+            decode = (lambda s: urlparse.unquote(str(s).replace('+', ' '))
+                      .decode('utf-8'))
+            name = decode(name)
+            if value is not None:
+                value = decode(value)
+
+            result.append((name, value))
+        return result
 
     @property
     def dict(self):
