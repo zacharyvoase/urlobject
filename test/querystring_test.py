0850fb04a485017ba31c95c1f93f330ccfc6e7d0
Zachary Voase <z@zacharyvoase.com>
2012-02-14 22:02:18 +0000

    Added more QueryString test cases.

--- test/querystring_test.py
+++ test/querystring_test.py
@@ -11,6 +11,9 @@ class QueryStringTest(unittest.TestCase):
     def test_preserves_hash_value_of_original_string(self):
         assert hash(QueryString(u'abc=123')) == hash(u'abc=123')
 
+    def test_list_returns_an_empty_list_for_empty_QueryStrings(self):
+        assert QueryString(u'').list == []
+
     def test_list_correctly_splits_on_ampersands(self):
         assert QueryString(u'abc=123&def=456').list == [
             (u'abc', u'123'), (u'def', u'456')]
@@ -36,20 +39,26 @@ class QueryStringTest(unittest.TestCase):
         assert QueryString(u'abc=123;abc=456').list == [
             (u'abc', u'123'), (u'abc', u'456')]
 
-    def test_list_returns_none_as_the_value_for_valueless_parameters(self):
+    def test_list_uses_none_as_the_value_for_valueless_parameters(self):
         assert QueryString(u'abc').list == [(u'abc', None)]
         assert QueryString(u'abc=123&def&ghi=456').list == [
             (u'abc', u'123'), (u'def', None), (u'ghi', u'456')]
 
-    def test_list_returns_empty_string_for_empty_valued_parameters(self):
+    def test_list_uses_empty_string_for_empty_valued_parameters(self):
         assert QueryString(u'abc=').list == [(u'abc', u'')]
         assert QueryString(u'abc=123&def=&ghi=456').list == [
             (u'abc', u'123'), (u'def', u''), (u'ghi', u'456')]
 
-    def test_list_returns_empty_string_for_empty_named_parameters(self):
+    def test_list_uses_empty_string_for_anonymous_parameters(self):
         assert QueryString(u'=123').list == [(u'', u'123')]
         assert QueryString(u'abc=123&=456&ghi=789').list == [
             (u'abc', u'123'), (u'', u'456'), (u'ghi', u'789')]
+
+    def test_list_can_handle_void_parameters(self):
+        assert QueryString(u'abc=123&&def=456').list == [
+            (u'abc', u'123'), (u'', None), (u'def', u'456')]
+        assert QueryString(u'abc=123&=&def=456').list == [
+            (u'abc', u'123'), (u'', u''), (u'def', u'456')]
 
     def test_dict_returns_a_dictionary_with_one_value_per_key(self):
         assert QueryString(u'abc=123&abc=456').dict == {u'abc': u'456'}
