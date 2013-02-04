c8b8972d4634a6f8cf6cdf2b3b7c59fb1d60f850
Aron Griffis <aron@arongriffis.com>
2013-02-04 14:57:56 -0500

    py3: Use relative imports

--- urlobject/urlobject.py
+++ urlobject/urlobject.py
@@ -1,9 +1,9 @@
 import urlparse
 
-from netloc import Netloc
-from path import URLPath, path_encode, path_decode
-from ports import DEFAULT_PORTS
-from query_string import QueryString
+from .netloc import Netloc
+from .path import URLPath, path_encode, path_decode
+from .ports import DEFAULT_PORTS
+from .query_string import QueryString
 
 
 class URLObject(unicode):
