6bc4983cd1d2c44947d43d734b565ae4153921da
Zachary Voase <z@zacharyvoase.com>
2013-02-18 17:25:24 +0000

    Bumped to v2.3.3.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='URLObject',
-    version='2.3.2',
+    version='2.3.3',
     description='A utility class for manipulating URLs.',
     author='Zachary Voase',
     author_email='z@zacharyvoase.com',
