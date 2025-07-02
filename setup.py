3d414bd8d6896d1548942a540a026490de12741f
Jonathan Reeves <Jonathan.Reeves@equifax.com>
2025-07-02 18:17:47 +0000

    install with pyproject toml file

--- setup.py
+++ setup.py
@@ -1,29 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+import setuptools
 
-
-setup(
-    name='URLObject',
-    version='2.4.3',
-    description='A utility class for manipulating URLs.',
-    author='Zachary Voase',
-    author_email='z@zacharyvoase.com',
-    url='http://github.com/zacharyvoase/urlobject',
-    packages=find_packages(exclude=('test',)),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: Public Domain',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup()
